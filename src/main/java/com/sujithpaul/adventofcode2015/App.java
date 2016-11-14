ce1a65eab6d535880fa2a4bae7931bbe0b2c8f8a
Sujith Paul <sujithpl@hotmail.com>
2016-11-13 23:41:59 -0500

    Start project

---
+++ src/main/java/com/sujithpaul/adventofcode2015/App.java
@@ -0,0 +1,13 @@
+package com.sujithpaul.adventofcode2015;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

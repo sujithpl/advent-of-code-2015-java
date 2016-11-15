373ac9313c082bc6fa04ff0360b8ea2af39079b1
Sujith Paul <sujithpl@hotmail.com>
2016-11-14 21:28:59 -0500

    Java 7 solution to Day 1

--- src/main/java/com/sujithpaul/adventofcode2015/Day1.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day1.java
@@ -1,13 +1,71 @@
 package com.sujithpaul.adventofcode2015;
 
-/**
- * Hello world!
- *
- */
-public class Day1 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+import java.util.Scanner;
+
+public class Day1 {
+
+	private String getFile(String fileName) {
+
+		StringBuilder result = new StringBuilder("");
+
+		// Get file from resources folder
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(fileName).getFile());
+
+		try (Scanner scanner = new Scanner(file)) {
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				result.append(line).append("\n");
+			}
+
+			scanner.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result.toString();
+
+	}
+
+	public int finalFloorPosition(String str) {
+		int floor = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '(')
+				floor++;
+			else if (str.charAt(i) == ')')
+				floor--;
+		}
+
+		return floor;
+	}
+
+	public int basementEntryPoint(String str) {
+		int floor = 0;
+		int step = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '(')
+				floor++;
+			else if (str.charAt(i) == ')')
+				floor--;
+			if (floor == -1) {
+				step = i + 1;
+				break;
+			}
+		}
+
+		return step;
+	}
+
+	public static void main(String[] args) {
+		Day1 obj = new Day1();
+		String floorInstructions = obj.getFile("files/day1-input.txt");
+		System.out.println("Final floor position is " + obj.finalFloorPosition(floorInstructions));
+		System.out.println("First time entering basement at step " + obj.basementEntryPoint(floorInstructions));
+	}
 }

98a89bfcd84c13405d4bdc44cd20af0a7daf191e
Sujith Paul <sujithpl@hotmail.com>
2016-11-26 14:04:10 -0500

    Complete Day 2 with refactor of utility class

--- src/main/java/com/sujithpaul/adventofcode2015/Day2.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day2.java
@@ -1,19 +1,35 @@
 package com.sujithpaul.adventofcode2015;
 
+import java.util.Arrays;
+
+import com.sujithpaul.adventofcode2015.utilities.InputProcessor;
+
 public class Day2 {
 
-	public int areaOfWrappingPaper(int side1, int side2, int side3) {
-		int area1 = side1 * side2;
-		int area2 = side2 * side3;
-		int area3 = side3 * side1;
+	public static int areaOfWrappingPaper(int[] sides) {
+		int area1 = sides[0] * sides[1];
+		int area2 = sides[1] * sides[2];
+		int area3 = sides[2] * sides[0];
 		int minArea = Math.min(area1, Math.min(area2, area3));
 		return minArea + 2 * (area1 + area2 + area3);
 	}
 
+	public static int lengthOfRibbon(int[] sides) {
+		Arrays.sort(sides);
+		int wrapLength = 2 * (sides[0] + sides[1]);
+		int bowLength = sides[0] * sides[1] * sides[2];
+		return wrapLength + bowLength;
+	}
+
 	public static void main(String[] args) {
-		Day2 obj = new Day2();
-		System.out.println(obj.areaOfWrappingPaper(2, 3, 4));
-		System.out.println(obj.areaOfWrappingPaper(1, 1, 10));
+		int sqftOfWrappingPaper = InputProcessor.readFile("day2-input.txt").map(s -> s.split("x"))
+				.map(InputProcessor::convertStringsArrayToIntsArray).map(Day2::areaOfWrappingPaper)
+				.mapToInt(Integer::intValue).sum();
+		System.out.println("Total square feet of wrapping paper: " + sqftOfWrappingPaper);
+		int feetOfRibbon = InputProcessor.readFile("day2-input.txt").map(s -> s.split("x"))
+				.map(InputProcessor::convertStringsArrayToIntsArray).map(Day2::lengthOfRibbon)
+				.mapToInt(Integer::intValue).sum();
+		System.out.println("Total feet of wrapping paper: " + feetOfRibbon);
 	}
 
 }
--- src/main/java/com/sujithpaul/adventofcode2015/utilities/InputProcessor.java
+++ src/main/java/com/sujithpaul/adventofcode2015/utilities/InputProcessor.java
@@ -6,13 +6,21 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class InputProcessor {
-	public Stream<String> readFile(String path) {
+	public static Stream<String> readFile(String filename) {
 		Stream<String> strStream = null;
 		try {
-			strStream = Files.lines(Paths.get(path));
+			strStream = Files.lines(Paths.get("src/main/resources/files/" + filename));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return strStream;
+	}
+
+	public static int[] convertStringsArrayToIntsArray(String[] stringArray) {
+		int[] intArray = new int[stringArray.length];
+		for (int i = 0; i < stringArray.length; i++) {
+			intArray[i] = Integer.parseInt(stringArray[i]);
+		}
+		return intArray;
 	}
 }

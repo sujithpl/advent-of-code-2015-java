38585b6692f76dd46c86383910a0d736f2d5b86d
Sujith Paul <sujithpl@hotmail.com>
2016-12-17 15:10:42 -0500

    Refactor into methods and solve part 2

--- src/main/java/com/sujithpaul/adventofcode2015/Day4.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day4.java
@@ -5,7 +5,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class Day4 {
 
-	public static String mda5(String input) throws NoSuchAlgorithmException {
+	public static String getMD5Hash(String input) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("MD5");
 		md.update(input.getBytes());
 		byte[] digest = md.digest();
@@ -16,14 +16,13 @@ public class Day4 {
 		return sb.toString();
 	}
 
-	public static void main(String[] args) {
-		String secret = "bgvyzdsv";
+	public static int mineAdventCoin(String secret, String startingSequence) {
 		boolean found = false;
 		int i = 0;
 		do {
 			String input = secret + i;
 			try {
-				if (mda5(input).substring(0, 5).equals("00000")) {
+				if (getMD5Hash(input).substring(0, startingSequence.length()).equals(startingSequence)) {
 					found = true;
 				}
 			} catch (NoSuchAlgorithmException e) {
@@ -31,7 +30,12 @@ public class Day4 {
 			}
 			i++;
 		} while (!found);
-		System.out.println(i-1);
+		return i - 1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Input for coin 1: " + mineAdventCoin("bgvyzdsv", "00000"));
+		System.out.println("Input for coin 2: " + mineAdventCoin("bgvyzdsv", "000000"));
 
 	}
 

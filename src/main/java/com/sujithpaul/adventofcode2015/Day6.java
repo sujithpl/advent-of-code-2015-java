bd3c4df3e99acdabd64a6d574e645170916e4b3f
Sujith Paul <sujithpl@hotmail.com>
2017-01-30 08:23:46 -0500

    Start Day 6 challenge

--- src/main/java/com/sujithpaul/adventofcode2015/Day6.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day6.java
@@ -2,6 +2,45 @@ package com.sujithpaul.adventofcode2015;
 
 public class Day6 {
 
+	private static int lightGrid[][];
+	private static int numberOfRows;
+	private static int numberOfColumns;
+
+	public Day6(int rows, int cols) {
+		numberOfRows = rows;
+		numberOfColumns = cols;
+		lightGrid = new int[numberOfRows][numberOfColumns];
+		this.resetAllLights();
+	}
+
+	public void resetAllLights() {
+		for (int row = 0; row < numberOfRows; row++) {
+			for (int col = 0; col < numberOfColumns; col++) {
+				lightGrid[row][col] = 0;
+			}
+		}
+	}
+
+	public int countNumberOfLitLights() {
+		int numOfLitLights = 0;
+		for (int row = 0; row < numberOfRows; row++) {
+			for (int col = 0; col < numberOfColumns; col++) {
+				if (lightGrid[row][col] == 1) {
+					numOfLitLights++;
+				}
+			}
+		}
+		return numOfLitLights;
+	}
+
+	public void turnOnLights(int leftRow, int leftColumn, int rightRow, int rightColumn) {
+		for (int row = leftRow; row <= rightRow; row++) {
+			for (int col = leftColumn; col <= rightColumn; col++) {
+				lightGrid[row][col] = 1;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
---
+++ src/test/java/com/sujithpaul/adventofcode2015/Day6Test.java
@@ -0,0 +1,34 @@
+package com.sujithpaul.adventofcode2015;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Day6Test {
+
+	private static Day6 testLightGrid;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		testLightGrid = new Day6(10, 10);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		testLightGrid.resetAllLights();
+	}
+
+	@Test
+	public void testCountNumberOfLitLights() {
+		assertEquals(0, testLightGrid.countNumberOfLitLights());
+	}
+
+	@Test
+	public void testTurnOnLights() {
+		testLightGrid.turnOnLights(1, 1, 8, 8);
+		assertEquals(64, testLightGrid.countNumberOfLitLights());
+	}
+
+}

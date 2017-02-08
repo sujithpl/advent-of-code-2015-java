3071c6346d09ad68f98e996ac68356a376730e2f
Sujith Paul <sujithpl@hotmail.com>
2017-02-08 08:09:38 -0500

    Refactor code and tests for Day 6 challenge

--- src/main/java/com/sujithpaul/adventofcode2015/Day6.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day6.java
@@ -19,6 +19,18 @@ public class Day6 {
 		this.resetAllLights();
 	}
 
+	public static Function<Integer, Integer> turnOff = i -> 0;
+
+	public static Function<Integer, Integer> turnOn = i -> 1;
+
+	public static Function<Integer, Integer> toggle = i -> i == 1 ? 0 : 1;
+
+	public static Function<Integer, Integer> turnUp = i -> ++i;
+
+	public static Function<Integer, Integer> turnDown = i -> i == 0 ? 0 : --i;
+
+	public static Function<Integer, Integer> turnUpTwice = i -> i += 2;
+
 	public void operateSetOfLights(int leftRow, int leftColumn, int rightRow, int rightColumn,
 			Function<Integer, Integer> operation) {
 		for (int row = leftRow; row <= rightRow; row++) {
@@ -28,44 +40,14 @@ public class Day6 {
 		}
 	}
 
-	private static Function<Integer, Integer> turnOff = i -> 0;
-
-	private static Function<Integer, Integer> turnOn = i -> 1;
-
-	private static Function<Integer, Integer> toggle = i -> i == 1 ? 0 : 1;
-
-	public void turnOnLights(int leftRow, int leftColumn, int rightRow, int rightColumn) {
-		this.operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnOn);
-	}
-
-	public void turnOffLights(int leftRow, int leftColumn, int rightRow, int rightColumn) {
-		this.operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnOff);
-	}
-
-	public void toggleLights(int leftRow, int leftColumn, int rightRow, int rightColumn) {
-		this.operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, toggle);
-	}
-
 	public void resetAllLights() {
 		this.operateSetOfLights(0, 0, numberOfRows - 1, numberOfColumns - 1, turnOff);
-	}
-
-	public int countNumberOfLitLights() {
-		int numOfLitLights = 0;
-		for (int row = 0; row < numberOfRows; row++) {
-			for (int col = 0; col < numberOfColumns; col++) {
-				if (lightGrid[row][col] == 1) {
-					numOfLitLights++;
-				}
-			}
-		}
-		return numOfLitLights;
 	}
 
 	private static final Pattern lightingInstructionPattern = Pattern
 			.compile("(.+) (\\d+),(\\d+) through (\\d+),(\\d+)");
 
-	public void processLightingInstruction(String instruction) {
+	public void processLightingInstruction(String instruction, int ruleSet) {
 		Matcher matcher = lightingInstructionPattern.matcher(instruction);
 		if (matcher.find()) {
 			String operation = matcher.group(1);
@@ -73,52 +55,36 @@ public class Day6 {
 			int leftColumn = Integer.parseInt(matcher.group(3));
 			int rightRow = Integer.parseInt(matcher.group(4));
 			int rightColumn = Integer.parseInt(matcher.group(5));
+			if (ruleSet == 1) {
+				switch (operation) {
+				case "turn on":
+					operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnOn);
+					break;
+				case "turn off":
+					operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnOff);
+					break;
+				case "toggle":
+					operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, toggle);
+					break;
+				default:
+					break;
+				}
+			} else {
+				switch (operation) {
+				case "turn on":
+					operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnUp);
+					break;
+				case "turn off":
+					operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnDown);
+					break;
+				case "toggle":
+					operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnUpTwice);
+					break;
+				default:
+					break;
+				}
 
-			switch (operation) {
-			case "turn on":
-				turnOnLights(leftRow, leftColumn, rightRow, rightColumn);
-				break;
-			case "turn off":
-				turnOffLights(leftRow, leftColumn, rightRow, rightColumn);
-				break;
-			case "toggle":
-				toggleLights(leftRow, leftColumn, rightRow, rightColumn);
-				break;
-			default:
-				break;
 			}
-		}
-	}
-
-	private static Function<Integer, Integer> turnUp = i -> ++i;
-
-	private static Function<Integer, Integer> turnDown = i -> i == 0 ? 0 : --i;
-
-	private static Function<Integer, Integer> turnUpTwice = i -> i += 2;
-
-	public void processLightingInstructionPartTwo(String instruction) {
-		Matcher matcher = lightingInstructionPattern.matcher(instruction);
-		if (matcher.find()) {
-			String operation = matcher.group(1);
-			int leftRow = Integer.parseInt(matcher.group(2));
-			int leftColumn = Integer.parseInt(matcher.group(3));
-			int rightRow = Integer.parseInt(matcher.group(4));
-			int rightColumn = Integer.parseInt(matcher.group(5));
-
-			switch (operation) {
-			case "turn on":
-				operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnUp);
-				break;
-			case "turn off":
-				operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnDown);
-				break;
-			case "toggle":
-				operateSetOfLights(leftRow, leftColumn, rightRow, rightColumn, turnUpTwice);
-				break;
-			default:
-				break;
-			}
-
 		}
 	}
 
@@ -126,7 +92,7 @@ public class Day6 {
 		int value = 0;
 		for (int row = 0; row < numberOfRows; row++) {
 			for (int col = 0; col < numberOfColumns; col++) {
-					value += lightGrid[row][col];
+				value += lightGrid[row][col];
 			}
 		}
 		return value;
@@ -134,11 +100,12 @@ public class Day6 {
 
 	public static void main(String[] args) {
 		Day6 day6 = new Day6(1000, 1000);
-		InputProcessor.readFile("files/day6-input.txt").forEach(str -> day6.processLightingInstruction(str));
+		InputProcessor.readFile("files/day6-input.txt").forEach(str -> day6.processLightingInstruction(str, 1));
 		System.out.println("Number of lit lights according to part 1 rules: " + day6.calculateTotalValueOfLights());
 		day6.resetAllLights();
-		InputProcessor.readFile("files/day6-input.txt").forEach(str -> day6.processLightingInstructionPartTwo(str));
-		System.out.println("Total Brightness of lights according to part 2 rules: " + day6.calculateTotalValueOfLights());
+		InputProcessor.readFile("files/day6-input.txt").forEach(str -> day6.processLightingInstruction(str, 2));
+		System.out
+				.println("Total Brightness of lights according to part 2 rules: " + day6.calculateTotalValueOfLights());
 	}
 
 }
--- src/test/java/com/sujithpaul/adventofcode2015/Day6Test.java
+++ src/test/java/com/sujithpaul/adventofcode2015/Day6Test.java
@@ -21,39 +21,79 @@ public class Day6Test {
 	}
 
 	@Test
-	public void testCountNumberOfLitLights() {
-		assertEquals(0, testLightGrid.countNumberOfLitLights());
+	public void testCalculateTotalValueOfLights() {
+		assertEquals(0, testLightGrid.calculateTotalValueOfLights());
+	}
+
+	@Test
+	public void testResetAllLights() {
+		testLightGrid.operateSetOfLights(1, 1, 8, 8, Day6.turnOn);
+		assertEquals(64, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.resetAllLights();
+		assertEquals(0, testLightGrid.calculateTotalValueOfLights());
 	}
 
 	@Test
 	public void testTurnOnLights() {
-		testLightGrid.turnOnLights(1, 1, 8, 8);
-		assertEquals(64, testLightGrid.countNumberOfLitLights());
+		testLightGrid.operateSetOfLights(1, 1, 8, 8, Day6.turnOn);
+		assertEquals(64, testLightGrid.calculateTotalValueOfLights());
 	}
 
 	@Test
 	public void testTurnOffLights() {
-		testLightGrid.turnOnLights(0, 0, 8, 8);
-		assertEquals(81, testLightGrid.countNumberOfLitLights());
-		testLightGrid.turnOffLights(4, 4, 8, 8);
-		assertEquals(56, testLightGrid.countNumberOfLitLights());
+		testLightGrid.operateSetOfLights(0, 0, 8, 8, Day6.turnOn);
+		assertEquals(81, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.operateSetOfLights(4, 4, 8, 8, Day6.turnOff);
+		assertEquals(56, testLightGrid.calculateTotalValueOfLights());
 	}
 
 	@Test
 	public void testToggleLights() {
-		testLightGrid.turnOnLights(0, 0, 6, 7);
-		assertEquals(56, testLightGrid.countNumberOfLitLights());
-		testLightGrid.toggleLights(3, 4, 8, 8);
-		assertEquals(54, testLightGrid.countNumberOfLitLights());
+		testLightGrid.operateSetOfLights(0, 0, 6, 7, Day6.turnOn);
+		assertEquals(56, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.operateSetOfLights(3, 4, 8, 8, Day6.toggle);
+		assertEquals(54, testLightGrid.calculateTotalValueOfLights());
 	}
 
 	@Test
-	public void testProcessLightingInstruction(){
-		testLightGrid.processLightingInstruction("turn on 0,0 through 4,9");
-		assertEquals(50, testLightGrid.countNumberOfLitLights());
-		testLightGrid.processLightingInstruction("turn off 0,6 through 9,9");
-		assertEquals(30, testLightGrid.countNumberOfLitLights());
-		testLightGrid.processLightingInstruction("toggle 1,4 through 7,8");
-		assertEquals(49, testLightGrid.countNumberOfLitLights());
+	public void testTurnUpLights() {
+		testLightGrid.operateSetOfLights(1, 1, 8, 8, Day6.turnUp);
+		assertEquals(64, testLightGrid.calculateTotalValueOfLights());
+	}
+
+	@Test
+	public void testTurnDownLights() {
+		testLightGrid.operateSetOfLights(0, 0, 8, 8, Day6.turnUp);
+		assertEquals(81, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.operateSetOfLights(4, 4, 9, 9, Day6.turnDown);
+		assertEquals(56, testLightGrid.calculateTotalValueOfLights());
+	}
+
+	@Test
+	public void testTurnUpTwiceLights() {
+		testLightGrid.operateSetOfLights(0, 0, 6, 7, Day6.turnUp);
+		assertEquals(56, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.operateSetOfLights(3, 4, 8, 8, Day6.turnUpTwice);
+		assertEquals(116, testLightGrid.calculateTotalValueOfLights());
+	}
+
+	@Test
+	public void testProcessLightingInstructionRuleSet1(){
+		testLightGrid.processLightingInstruction("turn on 0,0 through 4,9", 1);
+		assertEquals(50, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.processLightingInstruction("turn off 0,6 through 9,9", 1);
+		assertEquals(30, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.processLightingInstruction("toggle 1,4 through 7,8", 1);
+		assertEquals(49, testLightGrid.calculateTotalValueOfLights());
+	}
+
+	@Test
+	public void testProcessLightingInstructionRuleSet2(){
+		testLightGrid.processLightingInstruction("turn on 0,0 through 4,9", 2);
+		assertEquals(50, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.processLightingInstruction("turn off 0,6 through 9,9", 2);
+		assertEquals(30, testLightGrid.calculateTotalValueOfLights());
+		testLightGrid.processLightingInstruction("toggle 1,4 through 7,8", 2);
+		assertEquals(100, testLightGrid.calculateTotalValueOfLights());
 	}
 }

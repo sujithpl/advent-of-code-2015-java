a9f2def06f7abd7871437821d0c1fb4af17e6851
Sujith Paul <sujithpl@hotmail.com>
2017-02-13 08:21:22 -0500

    Add regex parser to Day 7 challenge code

--- src/main/java/com/sujithpaul/adventofcode2015/Day7.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day7.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day7 {
 
@@ -23,18 +25,84 @@ public class Day7 {
 		return value;
 	}
 
+	/*
+	 * Converts 32 bit integer in two's complement notation to the equivalent 16
+	 * bit short
+	 */
+	public static int getIntValue(String key) {
+		return 0xffff & circuit.get(key);
+	}
+
 	static Function<String, Integer> notOperation = s -> (~getValue(s));
 
 	static BiFunction<String, String, Integer> andOperation = (arg1, arg2) -> (getValue(arg1) & getValue(arg2));
-	
+
+	static BiFunction<String, String, Integer> orOperation = (arg1, arg2) -> (getValue(arg1) | getValue(arg2));
+
+	static BiFunction<String, String, Integer> lShiftOperation = (arg1, arg2) -> (getValue(arg1) << getValue(arg2));
+
+	static BiFunction<String, String, Integer> rShiftOperation = (arg1, arg2) -> (getValue(arg1) << getValue(arg2));
+
+	private static final Pattern assignmentInstructionPattern = Pattern.compile("(.+) -> (.+)");
+
+	private static final Pattern singleArgumentInstructionPattern = Pattern.compile("(.+) (.+) -> (.+)");
+
+	private static final Pattern doubleArgumentInstructionPattern = Pattern.compile("(.+) (.+) (.+) -> (.+)");
+
+	public static void processWiringInstruction(String instruction) {
+		Matcher matcher = doubleArgumentInstructionPattern.matcher(instruction);
+		if (matcher.find()) {
+			String arg1 = matcher.group(1);
+			String operation = matcher.group(2);
+			String arg2 = matcher.group(3);
+			String arg3 = matcher.group(4);
+			switch (operation) {
+			case "AND":
+				setValue(arg3, andOperation.apply(arg1, arg2));
+				break;
+			case "OR":
+				setValue(arg3, orOperation.apply(arg1, arg2));
+				break;
+			case "LSHIFT":
+				setValue(arg3, lShiftOperation.apply(arg1, arg2));
+				break;
+			case "RSHIFT":
+				setValue(arg3, rShiftOperation.apply(arg1, arg2));
+				break;
+			default:
+				break;
+			}
+			return;
+		}
+
+		Matcher matcher1 = singleArgumentInstructionPattern.matcher(instruction);
+		if (matcher1.find()) {
+			String operation = matcher.group(1);
+			String arg1 = matcher.group(2);
+			String arg2 = matcher.group(3);
+			if (operation.equals("NOT")) {
+				setValue(arg2, notOperation.apply(arg1));
+			}
+			return;
+		}
+
+		Matcher matcher2 = assignmentInstructionPattern.matcher(instruction);
+		if (matcher2.find()) {
+			String arg1 = matcher.group(1);
+			String arg2 = matcher.group(2);
+			setValue(arg2, getValue(arg1));
+			return;
+		}
+	}
+
 	public static void main(String[] args) {
-		Day7 obj = new Day7();
-		obj.setValue("ab", 3);
-		obj.setValue("de", 5);
-		System.out.println(obj.getValue("ab"));
-		System.out.println(obj.getValue("23"));
-		System.out.println(notOperation.apply("ab"));
-		System.out.println(andOperation.apply("ab", "de"));
+		// Day7 obj = new Day7();
+		Day7.setValue("ab", 3);
+		Day7.setValue("de", 5);
+		System.out.println(Day7.getValue("ab"));
+		System.out.println(Day7.getValue("23"));
+		System.out.println(0xffff & notOperation.apply("ab"));
+		System.out.println(0xffff & andOperation.apply("ab", "de"));
 
 	}
 

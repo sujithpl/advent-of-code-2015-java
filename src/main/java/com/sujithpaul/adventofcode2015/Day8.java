32a315d26f27b51b6728f18b2fbfbcd47b4f195b
Sujith Paul <sujithpl@hotmail.com>
2017-02-21 07:54:54 -0500

    Start Day 8 challenge

---
+++ src/main/java/com/sujithpaul/adventofcode2015/Day8.java
@@ -0,0 +1,12 @@
+package com.sujithpaul.adventofcode2015;
+
+public class Day8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "ab\0x27c";
+		System.out.println(s + ": " + s.length());
+
+	}
+
+}

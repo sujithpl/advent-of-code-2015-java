2c25898d79ed0a3af8be05cdb0f89ad58e54a2ff
Sujith Paul <sujithpl@hotmail.com>
2017-02-22 07:57:50 -0500

    Solve part 1 of Day 8 challenge

--- src/main/java/com/sujithpaul/adventofcode2015/Day8.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day8.java
@@ -1,12 +1,25 @@
 package com.sujithpaul.adventofcode2015;
 
+import com.sujithpaul.adventofcode2015.utilities.InputProcessor;
+
 public class Day8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String s = "ab\0x27c";
-		System.out.println(s + ": " + s.length());
-
+		int numberOfTextChars = InputProcessor.readFile("files/day8-input.txt") //
+				.mapToInt(s -> s.length()) //
+				.sum();
+		System.out.println(numberOfTextChars);
+		int numberOfInMemoryChars = InputProcessor.readFile("files/day8-input.txt") //
+				.map(s -> s.replaceAll("^\"", "")) //
+				.map(s -> s.replaceAll("\"$", "")) //
+				.map(s -> s.replaceAll("\\\\", "@")) //
+				.map(s -> s.replaceAll("@\\\"", "!")) //
+				.map(s -> s.replaceAll("@@", "#")) //
+				.map(s -> s.replaceAll("@x..", "%")) //
+				.mapToInt(s -> s.length()) //
+				.sum();
+		System.out.println(numberOfInMemoryChars);
+		System.out.println("Diff: " + (numberOfTextChars - numberOfInMemoryChars));
 	}
 
 }

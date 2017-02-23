7226e08b4db1760a4dbe002371e469bb63193a4a
Sujith Paul <sujithpl@hotmail.com>
2017-02-23 08:24:19 -0500

    Refactor part 1 of Day 8 challenge

--- src/main/java/com/sujithpaul/adventofcode2015/Day8.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day8.java
@@ -1,23 +1,46 @@
 package com.sujithpaul.adventofcode2015;
 
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Stream;
+
 import com.sujithpaul.adventofcode2015.utilities.InputProcessor;
 
 public class Day8 {
 
+	static Function<String, String> removeStartingQuotes = s -> s.replaceAll("^\"", "");
+
+	static Function<String, String> removeEndingQuotes = s -> s.replaceAll("\"$", "");
+
+	static Function<String, String> replaceBackSlashWithAt = s -> s.replaceAll("\\\\", "@");
+
+	static Function<String, String> replaceBackSlashAtWithExclamationMark = s -> s.replaceAll("@\\\"", "!");
+
+	static Function<String, String> replaceDoubleAtWithPound = s -> s.replaceAll("@@", "#");
+
+	static Function<String, String> replaceHexWithPercentageSign = s -> s.replaceAll("@x..", "%");
+
+	static Function<String, String> convertToInMemoryStringEquivalent = removeStartingQuotes //
+			.andThen(removeEndingQuotes) //
+			.andThen(replaceBackSlashWithAt) //
+			.andThen(replaceBackSlashAtWithExclamationMark) //
+			.andThen(replaceDoubleAtWithPound) //
+			.andThen(replaceHexWithPercentageSign);
+
+	static ToIntFunction<? super String> getStringLength = s -> s.length();
+
+	static int countCharsInStream(Stream<String> stringStream, Function<String, String> streamConverter) {
+		return stringStream //
+				.map(streamConverter) //
+				.mapToInt(getStringLength) //
+				.sum();
+	}
+
 	public static void main(String[] args) {
-		int numberOfTextChars = InputProcessor.readFile("files/day8-input.txt") //
-				.mapToInt(s -> s.length()) //
-				.sum();
+		int numberOfTextChars = countCharsInStream(InputProcessor.readFile("files/day8-input.txt"), (s) -> (s));
 		System.out.println(numberOfTextChars);
-		int numberOfInMemoryChars = InputProcessor.readFile("files/day8-input.txt") //
-				.map(s -> s.replaceAll("^\"", "")) //
-				.map(s -> s.replaceAll("\"$", "")) //
-				.map(s -> s.replaceAll("\\\\", "@")) //
-				.map(s -> s.replaceAll("@\\\"", "!")) //
-				.map(s -> s.replaceAll("@@", "#")) //
-				.map(s -> s.replaceAll("@x..", "%")) //
-				.mapToInt(s -> s.length()) //
-				.sum();
+		int numberOfInMemoryChars = countCharsInStream(InputProcessor.readFile("files/day8-input.txt"),
+				convertToInMemoryStringEquivalent);
 		System.out.println(numberOfInMemoryChars);
 		System.out.println("Diff: " + (numberOfTextChars - numberOfInMemoryChars));
 	}

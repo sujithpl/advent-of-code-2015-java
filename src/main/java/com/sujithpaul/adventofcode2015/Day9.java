80d9116ef2a3e0a2263e73e5b4d5bce07296f1b8
Sujith Paul <sujithpl@hotmail.com>
2017-12-23 10:31:50 +0530

    Complete Day 9 challenge

--- src/main/java/com/sujithpaul/adventofcode2015/Day9.java
+++ src/main/java/com/sujithpaul/adventofcode2015/Day9.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
 
 import com.sujithpaul.adventofcode2015.utilities.InputProcessor;
 import com.sujithpaul.adventofcode2015.utilities.ListHelper;
 
 public class Day9 {
 
-	private static Map<String, HashMap<String, Integer>> interCityMap = new HashMap<String, HashMap<String, Integer>>();
+	protected static Map<String, HashMap<String, Integer>> interCityMap = new HashMap<String, HashMap<String, Integer>>();
 
 	private static void addCitiesToMap(String city1, String city2, int distance) {
 		if (!interCityMap.containsKey(city1)) {
@@ -20,35 +19,48 @@ public class Day9 {
 		interCityMap.get(city1).put(city2, distance);
 	}
 
+	public static int getShortestRouteLength() {
+		return getAllPossibleRouteLengths().stream().min(Integer::compare).get();
+	}
+
+	public static int getLongestRouteLength() {
+		return getAllPossibleRouteLengths().stream().max(Integer::compare).get();
+	}
+
+	private static List<Integer> getAllPossibleRouteLengths() {
+		List<String> cities = new ArrayList<String>(interCityMap.keySet());
+		List<List<?>> allRoutesThroughTheCities = ListHelper.getAllPermutations(cities);
+		List<Integer> allRouteLengths = getRouteLengths(allRoutesThroughTheCities);
+		return allRouteLengths;
+	}
+
+	private static List<Integer> getRouteLengths(List<List<?>> allRoutes) {
+		List<Integer> result = new ArrayList<>();
+		for (List<?> aRoute : allRoutes) {
+			int routeLength = 0;
+			for (int i = 0; i < aRoute.size() - 1; i++) {
+				routeLength += getDistanceBetweenCities(aRoute.get(i).toString(), aRoute.get(i + 1).toString());
+			}
+			result.add(routeLength);
+		}
+		return result;
+	}
+
 	private static int getDistanceBetweenCities(String city1, String city2) {
 		return (int) interCityMap.get(city1).get(city2);
 	}
 
-	static BiFunction<String, String, Integer> k = (city1, city2) -> interCityMap.get(city1).get(city2);
-
 	public static void main(String[] args) {
+
 		InputProcessor.readFile("files/day9-input.txt") //
 				.map(str -> str.split(" ")) //
 				.forEach((str) -> {
 					addCitiesToMap(str[0], str[2], Integer.parseInt(str[4]));
 					addCitiesToMap(str[2], str[0], Integer.parseInt(str[4]));
 				});
-		List<String> cities = new ArrayList<String>(interCityMap.keySet());
-		List<List<?>> pathThroughCities = ListHelper.getAllPermutations(cities);
-		int minDist = Integer.MAX_VALUE;
-		int maxDist = Integer.MIN_VALUE;
-		for (List<?> aPath : pathThroughCities) {
-			int dist = 0;
-			for (int i = 0; i < aPath.size() - 1; i++) {
-				dist += getDistanceBetweenCities(aPath.get(i).toString(), aPath.get(i + 1).toString());
-			}
-			if (dist < minDist)
-				minDist = dist;
-			if (dist > maxDist)
-				maxDist = dist;
-		}
-		System.out.println(minDist);
-		System.out.println(maxDist);
+
+		System.out.println("Shortest Route Length (Part 1) is " + getShortestRouteLength());
+		System.out.println("Longest Route Length (Part 2) is " + getLongestRouteLength());
 	}
 
 }
--- src/test/java/com/sujithpaul/adventofcode2015/Day9Test.java
+++ src/test/java/com/sujithpaul/adventofcode2015/Day9Test.java
@@ -1,14 +1,37 @@
 package com.sujithpaul.adventofcode2015;
 
 import static org.junit.Assert.*;
+import static com.sujithpaul.adventofcode2015.Day9.*;
 
+import java.util.HashMap;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Day9Test {
 
+	@BeforeClass
+	public static void setUp() {
+		interCityMap = new HashMap<String, HashMap<String,Integer>>();
+		interCityMap.put("London", new HashMap<>());
+		interCityMap.get("London").put("Dublin", 464);
+		interCityMap.get("London").put("Belfast", 518);
+		interCityMap.put("Dublin", new HashMap<>());
+		interCityMap.get("Dublin").put("London", 464);
+		interCityMap.get("Dublin").put("Belfast", 141);
+		interCityMap.put("Belfast", new HashMap<>());
+		interCityMap.get("Belfast").put("London", 518);
+		interCityMap.get("Belfast").put("Dublin", 141);
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetShortestRouteLength() {
+		assertEquals(605, getShortestRouteLength());
+	}
+
+	@Test
+	public void testGetLongestRouteLength() {
+		assertEquals(982, getLongestRouteLength());
 	}
 
 }
